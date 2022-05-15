c5b87d8b5521f09a4ed656d88a4970e50dafff66
Daniel García Campoy <72436388+DanielGarciaCampoy@users.noreply.github.com>
2022-05-15 21:45:40 +0200

    Add files via upload

---
+++ JavaGit/src/main/java/JavaGit.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @author accio
+ */
+public class JavaGit {
+    public static void main(String[] args) {
+        System.out.println("holamundo");
+        System.out.println("segundo holamundo");
+        System.out.println("tercer holamundo");
+    }
+}
